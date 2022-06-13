005303ed3d56b8cab5e2e32d11968fdc2a9802f3
Funkschy <felix.schoeller@protonmail.com>
2022-06-13 20:48:15 +0200

    Added navigation
    - switched from material-ui to react native paper
    - added empty settings view with back button

--- src/main/weebmanager/core.cljs
+++ src/main/weebmanager/core.cljs
@@ -1,8 +1,11 @@
 (ns weebmanager.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require
-   ["@react-native-material/core" :as m]
    ["react-native" :as rn :refer [AppRegistry]]
+   ["react-native-paper" :as p]
+   ["@react-navigation/native" :as n]
+   ["@react-navigation/native-stack" :refer [createNativeStackNavigator]]
+   ["react-native-vector-icons/MaterialIcons" :as m]
    [cljs.core.async :refer [<!]]
    [reagent.core :as r]
    [weebmanager.anime :as a]))
@@ -10,47 +13,95 @@
 (defn pluralize [noun count]
   (str count " " noun (when (> count 1) "s")))
 
+(def MaterialIcon (. m -default))
+
+(defn back-icon []
+  (fn []
+    (r/as-element
+     [:> MaterialIcon
+      {:name "arrow-back"
+       :color "white"
+       :size 24}])))
+
+(defn header-bar []
+  (fn [^js navigation]
+    (let [route-name  (-> navigation .-route .-name)]
+      (r/as-element
+       [:> (. p/Appbar -Header)
+        (when (.-back navigation)
+          [:> (. p/Appbar -Action)
+           {:icon (back-icon)
+            :on-press #(-> navigation .-navigation .pop)}])
+
+        [:> (. p/Appbar -Content)
+         {:title route-name}]
+
+        (when-not (= route-name "Settings")
+          [:> (. p/Appbar -Action)
+           {:icon "dots-vertical"
+            :on-press #(-> navigation .-navigation (.push "Settings"))}])]))))
+
+(defn anime-icon [^js anime]
+  (fn []
+    (r/as-element
+     [:> (. p/Avatar -Image) {:source {:uri (-> anime .-item .-image)}}])))
+
 (defn anime-list-entry [^js anime]
   (r/as-element
-   [:> m/ListItem {:leading-mode "avatar"
-                   :leading (r/as-element
-                             [:> m/Avatar {:image {:uri (-> anime .-item .-image)}}])
-                   :title (-> anime .-item .-name)
-                   :secondary-text (str "You are "
-                                        (pluralize "episode" (-> anime .-item .-behind))
-                                        " behind")}]))
+   [:> (. p/List -Item)
+    {:left (anime-icon anime)
+     :title (-> anime .-item .-name)
+     :description (str "You are "
+                       (pluralize "episode" (-> anime .-item .-behind))
+                       " behind")}]))
+
+(defn main-screen [animes-atom]
+  (let [animes @animes-atom]
+    (fn []
+      (r/as-element
+       [:> rn/View
+        {:style {:flex 1
+                 :padding-top 0}}
+
+        [:> rn/FlatList
+         {:style {:margin 4
+                  :margin-bottom 40
+                  :align-self "stretch"}
+          :data (map (fn [{:keys [title main_picture behind]}]
+                       ;; TODO: add option to switch between en and jp
+                       {:id title
+                        :name title
+                        :behind behind
+                        :image (get main_picture :medium "")})
+                     animes)
+          :render-item anime-list-entry}]]))))
+
+(defn settings-screen []
+  (fn []
+    (r/as-element
+     [:> rn/View {:style {:flex 1
+                          :padding-top 0}}
+
+      [:> rn/Text "Settings"]])))
 
 (defn app-root []
-  (let [animes (r/atom [])]
+  (let [animes (r/atom [])
+        stack  (createNativeStackNavigator)]
     (go (reset! animes (<! (a/fetch-behind-schedule "funkschy"))))
     (fn []
-      [:> rn/View {:style {:flex 1
-                           :padding-top 0}}
-
-       [:> m/AppBar {:title "Weebmanager"
-                     :trailing
-                     (r/as-element [:> m/Button {:title "⫶"
-                                                 :on-press #(println "settings")
-                                                 :title-style {:font-weight "bold"
-                                                               :font-size 26}
-                                                 :style {:padding 0
-                                                         :border :none
-                                                         :justify-content :center
-                                                         :align-content :center
-                                                         :box-shadow :none
-                                                         :elevation 0}}])}]
-
-       [:> rn/FlatList {:style {:margin 4
-                                :margin-bottom 40
-                                :align-self "stretch"}
-                        :data (map (fn [{:keys [title main_picture behind]}]
-                                      ;; TODO: add option to switch between en and jp
-                                     {:id title
-                                      :name title
-                                      :behind behind
-                                      :image (get main_picture :medium "")})
-                                   @animes)
-                        :render-item anime-list-entry}]])))
+      [:> p/Provider
+       [:> n/NavigationContainer
+        [:> (. stack -Navigator)
+         {:initial-route-name "Weebmanager"
+          :screen-options {:header (header-bar)}}
+         [:> (. stack -Screen)
+          {:name "Weebmanager"
+           :component (main-screen animes)
+           :options {:animation "fade_from_bottom"}}]
+         [:> (. stack -Screen)
+          {:name "Settings"
+           :component (settings-screen)
+           :options {:animation "fade_from_bottom"}}]]]])))
 
 (defn start []
   (.registerComponent AppRegistry
